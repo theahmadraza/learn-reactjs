74296978420b84f23521e517f6405c9d35dc3a73
Muhammad Ahamd Raza <csmirza11@gmail.com>
2021-09-11 16:48:22 +0500

    using State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Expenses/ExpenseItem.css';
 import ExpenseDate from '../Expenses/ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
+    const [title, setTitle] = useState(props.title)
+    console.log("Outside " + title)
+
+    const changeHandler = () => {
+        setTitle("Updated!!!")
+        console.log("ChangeHandler " + title)
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date= {props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">
                     {props.amount}
                 </div>
             </div>
+            <button onClick={changeHandler}>Change Title</button>
         </ Card> 
     )
 }
