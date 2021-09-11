4fb50edb38efade1590c9f2ec2a8f79cd96ba329
Muhammad Ahamd Raza <csmirza11@gmail.com>
2021-09-11 14:10:12 +0500

    spliting components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: "956",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Tissue Paper",
+      amount: "$156",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "Car Wash",
+      amount: "$343",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Dresses",
+      amount: "$544",
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
   return (
     <div className="App">
-        <h2> Let Start Have</h2>
-        <ExpenseItem />
+      <h2> Let Start Have</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
